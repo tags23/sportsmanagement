0de89233efbe7aa788beb15c32da75ed7d796c3d
iceshore97 <mlkrasts@gmail.com>
2020-11-09 17:42:24 +0200

    Make an initial effort to use proper templating, some changes to titles in views

--- sms/views.py
+++ sms/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def login(request):
-    return render (request, 'sms/login.html')
+    return render(request, 'sms/login.html', context={"title": "SMS: Login"})
+
 
 def home(request):
-    return render (request, 'sms/home.html')
+    return render(request, 'sms/home.html', context={"title": "SMS: Home"})
