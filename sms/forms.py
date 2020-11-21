90978dc42c8138c524e955b68a5cf53ebed6ee34
Roberts <stnig7@gmail.com>
2020-11-21 19:08:40 +0200

    Added widgets to CompetitionForm \nCreated styling for form.html\nRefined form output logic

--- sms/forms.py
+++ sms/forms.py
@@ -6,3 +6,13 @@ class CompetitionForm(forms.ModelForm):
     class Meta:
         model = Competition
         fields = '__all__'
+
+        widgets = {
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control'}),
+            'time': forms.TimeInput(attrs={'class': 'form-control'}),
+            'sport': forms.TextInput(attrs={'class': 'form-control'}),
+            'comp_format': forms.TextInput(attrs={'class': 'form-control'}),
+            'comp_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'no_of_participants': forms.NumberInput(attrs={'class': 'form-control'})
+        }
